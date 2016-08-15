ca72f8e3db916703d1561e02dcd180741fc45db3
Dan Stoza <stoza@google.com>
2016-08-15 09:39:57 -0700

    r5x: Disable HWC virtual displays
    Disables hardware composer virtual displays, since they were indirectly
    causing screenrecord to jank.
    
    Bug: 30022738
    Change-Id: I6b209c4ba882fbbaf7cdd65ae412d53f1ada6bc9
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -136,7 +136,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     debug.mdpcomp.logs=0 \
     debug.sf.disable_backpressure=1 \
-    debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=0 \
     persist.sys.sf.color_saturation=1.0 \
