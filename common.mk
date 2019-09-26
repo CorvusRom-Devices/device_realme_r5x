79ac650f0dc975536bf023a58148a85ba7a71ead
Erfan Abdi <erfangplus@gmail.com>
2019-09-26 19:40:20 +0330

    sm6125-common: Write Fingerprint HAL Adapter
    vendor.oppo.hardware.biometrics.fingerprint@2.1 -> android.hardware.biometrics.fingerprint@2.1
    
    Change-Id: Id33db73425bb5e92ef5c45e2f8d40fa462942af3
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- common.mk
+++ common.mk
@@ -209,6 +209,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.RMX1911
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
