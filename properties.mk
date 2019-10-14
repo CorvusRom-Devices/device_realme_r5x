0141d4b0410dc12943fb83e8ed10560375bea490
Minchan Kim <minchan@google.com>
2019-10-14 14:25:22 -0700

    RMX1911: Enable zram writeback job scheduler
    Bug: 140882488
    Change-Id: I9deffcbe25474837d5167024297d06601530aae5
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- properties.mk
+++ properties.mk
@@ -236,3 +236,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
