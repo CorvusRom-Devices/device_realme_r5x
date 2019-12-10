524b0a22ce9495ed57f243c10d5d6c33260361bc
justinweng <justinweng@google.com>
2019-12-10 18:31:56 +0800

    r5x: audio: enforce 24-bit audio for offload playback
    Test: Audio Playback
    
    Change-Id: Ia581e65a0e5e8dcbfae188fe36ca9d07bd9eb4e2
    Signed-off-by: justinweng <justinweng@google.com>
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -6,6 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.bluetooth.a2dp_offload.disabled=false \
     persist.vendor.audio.ras.enabled=false \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
+    persist.vendor.audio_hal.dsp_bit_width_enforce_mode=24 \
     ro.af.client_heap_size_kbyte=7168 \
     ro.bluetooth.a2dp_offload.supported=true \
     ro.vendor.audio.sdk.fluencetype=none \
