2cca26e93c4ef8c0e0aceb14ca34eeb342f48226
Bruno Martins <bgcngm@gmail.com>
2020-10-28 22:32:08 +0000

    r5x: Ditch rild.libpath prop
    This is only needed for AOSP rild services. It's currently
    unused since the move to QCRIL.
    
    Change-Id: I5eb4d0bc2d086d5a2f29e3c3f5ee2d8f0220f5f5
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -197,8 +197,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.data_ltd_sys_ind=1 \
     persist.vendor.radio.data_con_rprt=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1 \
-    vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
+    persist.vendor.radio.sib16_support=1
 
 # Sensor
 PRODUCT_PROPERTY_OVERRIDES += \
