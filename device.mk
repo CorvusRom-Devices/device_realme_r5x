d4a644bbb7c10e351017b1f536d75e9d5cf10b91
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    RMX1911: Don't build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -402,10 +402,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# TextClassifier smart selection model files
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
