96cec7b713b30a195851856078a0fbfa6f5ffde3
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    RMX1911: Migrate to Health 2.1 HAL
    Change-Id: Ic6302844ac58c16713b43b6871a705b9b4749a96
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -223,9 +223,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
-# Healthd
+# Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
