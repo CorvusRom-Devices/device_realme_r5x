13680a4d53953c3badf2d9e079db5988d4f8f6c2
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-02-08 03:33:55 +0000

    sm6125-common: Drop framework manifest
    * Will move to compability matrix instead

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,7 +132,6 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 3.0
 
 # HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/configs/manifests/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/manifests/compatibility_matrix.xml
 
