3fd7f1c5583eb0567daf1e3593e3e84a1e4b1724
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    r5x: Switch to source built vendor.qti.hardware.perf@2.0
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -323,7 +323,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.0.vendor
 
 # Perf
 PRODUCT_COPY_FILES += \
