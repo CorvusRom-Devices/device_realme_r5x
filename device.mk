ae5c7790d5d788e7fe9e61b3df4648103b935c62
Zinadin Zidan <zidan44@pixelexperience.org>
2021-03-19 21:21:35 +0600

    r5x: Build power stats hardware service mock
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Id7ae9cbc563fad84d45706cbff85a9b9945a1680
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -323,7 +323,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Perf
 PRODUCT_PACKAGES += \
