db8c0e7920de5e6fcb4afd573a1581ba09726ee5
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-05-16 19:20:32 +0530

    r5x: Use clang for compiling kernel
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,9 +93,6 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_SOURCE := kernel/realme/r5x
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/r5x_defconfig
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
