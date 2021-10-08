2c53d19e7c4d5f709ddb79fe2cabcbe8766e9931
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-10-08 07:18:21 +0000

    r5x: Use powerhal 1.0

--- device.mk
+++ device.mk
@@ -335,8 +335,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl
 
 # Perf
 PRODUCT_PACKAGES += \
