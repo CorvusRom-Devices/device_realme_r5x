47a04e276e9bb2e80a98c6bfcdb543f3187c770d
Edwin Wong <>
2021-10-11 17:42:55 +0200

    r5x: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -188,7 +188,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
