b2f3b74f99a3822919d20f695f7d856a5effc7f0
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-10-20 13:15:02 +0000

    Revert "r5x: Use powerhal 1.0"
    This reverts commit 2c53d19e7c4d5f709ddb79fe2cabcbe8766e9931.

--- device.mk
+++ device.mk
@@ -344,8 +344,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service \
-    android.hardware.power@1.0-impl
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Perf
 PRODUCT_PACKAGES += \
