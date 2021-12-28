1be9164b76a9eefe8143193c9a3dfa835cf705e2
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-12-28 19:17:39 +0530

    Revert "Revert "Revert "r5x: Init aex"""
    This reverts commit 345010b910db191ea1f92739ad1a92eaafbf716a.

--- aosp_r5x.mk
+++ aosp_r5x.mk
@@ -20,14 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
-#TARGET_GAPPS_ARCH := arm64
-#TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_BRAND := Realme
