362b5a17b917fa987ab7850ecfe9e08defeea477
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-12-28 11:27:01 +0000

    Revert "r5x: Remove -qti from power hal"
    This reverts commit 5d8241463b4cbd3cd2c46cd6cd1a60d212fb6014.

--- device.mk
+++ device.mk
@@ -341,7 +341,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
+    android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
 # Perf
