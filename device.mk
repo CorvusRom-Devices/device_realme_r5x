7f52abca5d31cb4acc297ab64e378a9382ba35ff
kubersharma001 <kubersharma001@gmail.com>
2022-02-25 19:04:43 +0530

    r5x: Remove non-existent libldacBT_bco build rules
    - These seem to be long gone/not available to be source compiled?
    - Anyways, these seem to have no actual function and are not needed other than to avoid logspams
    
    unknown target 'libldacBT_bco'
    
    [partially taken from commit https://github.com/ArrowOS-Devices/android_device_xiaomi_vayu/commit/221e94a98a3fd3025ada8e2066da712fb6de974d]
    
    Change-Id: I47a0ed90c11210a8ebdec8c5a4685ec50da8ee0d

--- device.mk
+++ device.mk
@@ -94,7 +94,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbthost_if \
-    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
