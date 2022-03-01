f42607ba519defad9ff1b3d13d47834516415d64
Magicxavi <rolfpeterkesche@gmail.com>
2022-03-01 14:51:51 +0000

    r5x: power-libperfmgr: Adapt power service for r5x
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- power-libperfmgr/disp-power/DisplayLowPower.cpp
+++ power-libperfmgr/disp-power/DisplayLowPower.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.r5x-libperfmgr"
 
 #include <errno.h>
 #include <unistd.h>
