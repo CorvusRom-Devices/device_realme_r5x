4c4a51a6b143c57bee92000936f5e5c5a02a59d3
Mikhail Naganov <mnaganov@google.com>
2022-03-10 20:36:12 +0000

    r5x: configs: Uprev Audio HAL to v7
    Updated audio policy manager
    configuration files to comply with the
    changes in the XML schema.
    
    Bug: 142480271 Test: atest
    VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest# Date:      Thu Mar 10 20:36:12 2022 +0000

--- device.mk
+++ device.mk
@@ -47,8 +47,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio.service \
