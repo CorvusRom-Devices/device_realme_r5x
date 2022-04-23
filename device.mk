6a9e632ffca00b855af8bd64b028cd9f343a25f0
MAdMiZ <mizdrake7@gmail.com>
2022-04-24 03:15:39 +0530

    r5x: prebuilts: Let's Ship With A Music Player
    * Since AOSP doesn't have a good music player yet let's ship Xperia Music port which is highly preffered by users.
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -320,6 +320,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Muzix
+PRODUCT_PACKAGES += \
+    Muzix
+
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
