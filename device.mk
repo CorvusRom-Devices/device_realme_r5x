691879903593dbeba301b28156fd677b6768fe59
Alex Hong <rurumihong@google.com>
2022-05-13 08:11:07 +0545

    r5x: Drop Renderscipt HAL
    * Deprecated since Android12
    * Ref: https://developer.android.com/reference/android/renderscript/RenderScript
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -368,10 +368,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
