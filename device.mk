3eca36a855be5006ed395420d999b0d81b86d2bd
Arian <arian.kulmer@web.de>
2022-07-28 10:50:48 +0000

    r5x: gps: Build GPS HAL from source
    * slim_daemon is disabled in izat.conf, hence drop it.
    
    Change-Id: Ia1ed33f6250b898d149970389befe8033cc8eaf3
    Signed-off-by: MAdMiZ <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -220,13 +220,37 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor \
     libqti_vndfwk_detect.vendor
 
-# GPS / Location
+# GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss.measurement_corrections@1.1.vendor
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1.vendor \
     android.hardware.gnss@2.1-service-qti \
+    android.hardware.gnss@2.1-impl-qti
+
+PRODUCT_PACKAGES += \
     libbatching \
     libgeofencing \
     libgnss
+
+PRODUCT_PACKAGES += \
+    apdr.conf \
+    flp.conf \
+    gnss_antenna_info.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
+
+PRODUCT_PACKAGES += \
+    gnss@2.0-base.policy \
+    gnss@2.0-xtra-daemon.policy \
+    gnss@2.0-xtwifi-client.policy \
+    gnss@2.0-xtwifi-inet-agent.policy
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/gps/etc/,$(TARGET_COPY_OUT_VENDOR)/etc)
