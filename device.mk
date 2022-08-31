2589cbcf353bd792a76d250cecd5522f2656be1b
DarkJoker360 <simoespo159@gmail.com>
2022-08-31 11:00:37 +0000

    r5x: configs: Drop secure_element HAL
    * Unused.
    
    Change-Id: I40e583d0b29a01b28706eb4561c5a9b85a56f66f
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -591,7 +591,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.radio@1.5.vendor \
-    android.hardware.secure_element@1.2.vendor \
     android.hardwareundtrigger@2.3.vendor \
     android.hardware.usb.gadget@1.1.vendor \
     android.hardware.usb@1.2.vendor \
