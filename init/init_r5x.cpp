8dfa5bb6a27c3043dda5c258e16fac145fb62725
daniml3 <daniel@danielml.dev>
2022-09-03 14:37:28 +0000

    r5x: libinit: Recovery improvements
    * Always enable ro.debuggable
    * Enable adb by default
    
    Change-Id: I5d3bf5d8c1bbd3dda29d27a2f1e9fbd3d1d5383a
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- init/init_r5x.cpp
+++ init/init_r5x.cpp
@@ -45,6 +45,8 @@
 #include <android-base/properties.h>
 #include "vendor_init.h"
 
+#include <fs_mgr_dm_linear.h>
+
 using android::base::GetProperty;
 using android::base::ReadFileToString;
 
@@ -183,4 +185,12 @@ void init_device_model()
 void vendor_load_properties() {
     init_device_model();
     init_fp_properties();
+
+#ifdef __ANDROID_RECOVERY__
+    std::string buildtype = GetProperty("ro.build.type", "userdebug");
+    if (buildtype != "user") {
+        property_override("ro.debuggable", "1");
+        property_override("ro.adb.secure.recovery", "0");
+    }
+#endif
 }
