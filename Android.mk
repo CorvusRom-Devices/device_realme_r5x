b2483d926ac52167a8d7e9c309a3c02cd71bde8b
Arian <arian.kulmer@web.de>
2022-09-06 14:30:14 +0000

    r5x: makefile: Symlink wlan_mac.bin from persist to qca_cld firmware
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- Android.mk
+++ Android.mk
@@ -36,6 +36,13 @@ $(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
 
+WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin
+$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS MAC bin link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
+
 ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_MAC_SYMLINK)
 
 # A/B builds require us to create the mount points at compile time.
