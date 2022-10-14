b8a9690de804393ce5467941368111da7270a151
Sarthak Roy <sarthakroy2002@gmail.com>
2022-10-14 14:04:55 +0000

    r5x: extract-files: Use host patchelf
    * Instead of patchelf prebuilt binary
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I4c9d1e76d26afa223c62677c41b62bd68e067ba7

--- extract-files.sh
+++ extract-files.sh
@@ -56,10 +56,10 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/bin/hw/android.hardware.health@2.0-service.oppo)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            patchelf --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.health@2.0-service)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            patchelf --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
     esac
 }
