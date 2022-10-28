8914ff02d01697b7bd04ab9dac9aed8d90c1eef5
OdSazib <odsazib@gmail.com>
2022-10-28 16:01:32 +0000

    r5x: configs: Uprev android.hardwarecamera.provider to 2.6
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -122,10 +122,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.device@3.4 \
-    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libcamera2ndk_vendor \
